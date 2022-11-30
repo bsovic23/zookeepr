b519d0b32c1553b0aa7a57d7a3694aa9f64f932d
Brit Sovic <bsovic@tulane.edu>
2022-11-30 15:28:58 -0500

    updating api routes

--- server.js
+++ server.js
@@ -102,6 +102,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (rec, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (rec, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
